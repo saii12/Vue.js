59df089c161df9f985d7caf1673cd3dc10839f65
hansan <hansan9611@gmail.com>
2023-11-13 18:08:25 +0900

    실습예제

---
+++ ch06/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useMainStore } from "./store/mainStore";
+import MyComponent1 from "./components/MyComponent1.vue";
+import MyComponent2 from "./components/MyComponent2.vue";
+
+const mainStore = useMainStore();
+</script>
+
+<template>
+  <h3>ch06.Pinia 실습</h3>
+
+  <!-- useMainStore의 state 값 가져오기 -->
+  <p>mainStore title : {{ mainStore.title }}<br /></p>
+  <p>mainStore count : {{ mainStore.count }}<br /></p>
+
+  <p>cities : {{ mainStore.cities }}</p>
+
+  <table border="1">
+    <tr>
+      <td>아이디</td>
+      <td>이름</td>
+      <td>나이</td>
+    </tr>
+    <tr v-for="person in mainStore.persons">
+      <!-- for문 이렇게 표현한다! -->
+      <td>{{ person.uid }}</td>
+      <td>{{ person.name }}</td>
+      <td>{{ person.age }}</td>
+    </tr>
+  </table>
+
+  <MyComponent1 />
+  <MyComponent2 />
+</template>
+
+<style scoped></style>
---
+++ ch06/src/components/MyComponent1.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from "@vue/reactivity";
+import { useMainStore } from "../store/mainStore";
+
+// 전역스토어 가져오기
+const mainStore = useMainStore();
+
+// 전역스토어 getters 실행
+const count1 = mainStore.doubleCount;
+const count2 = computed(() => mainStore.doubleCount); // computed(재실행) 해줘야 count2 값 바뀜
+
+// 전역스토어 actions 실행
+const btnIncrement = () => {
+  mainStore.increment();
+};
+const btnDecrement = () => {
+  mainStore.decrement();
+};
+</script>
+
+<template>
+  <h4>MyComponent1</h4>
+
+  <p>
+    mainStore count1 : {{ count1 }}<br />
+    mainStore count2 : {{ count2 }}<br />
+  </p>
+  <button @click="btnIncrement">증가</button>
+  <button @click="btnDecrement">감소</button>
+</template>
+<style scoped></style>
